de7d486a3b2c551ca0801e2b619d27ec5cb05936
Samou0625 <kcharbelhounye@gmail.com>
2025-06-14 21:59:24 +0200

    Add files via upload

---
+++ Nouvelle_organisation/Frontend/motoshare/src/NewMessageForm.vue
@@ -0,0 +1,20 @@
+<template>
+  <form @submit.prevent="submit">
+    <input v-model="text" placeholder="Écris ton message..." />
+    <button type="submit">Envoyer</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data: () => ({ text: '' }),
+  methods: {
+    submit() {
+      if (this.text.trim()) {
+        this.$emit('send', this.text);
+        this.text = '';
+      }
+    }
+  }
+};
+</script>
