30444317b1dda389b6d051bd2ef44018e0522462
Brivice-07 <alladagbinbrivice@gmail.com>
2025-06-16 22:20:33 +0100

    Update config.py

--- Nouvelle_organisation/Backend/services/config.py
+++ Nouvelle_organisation/Backend/services/config.py
@@ -1,3 +1,4 @@
 class Config:
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Brivice@localhost/Ifri_comotorage'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    ORS_API_KEY = '5b3ce3597851110001cf624830dd089e936245beaae668bc47a6b315'
